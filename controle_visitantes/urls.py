488ec7f122c4bcd3a9178385f183e394720005f9
Noemi <87270671+noemimedeiros@users.noreply.github.com>
2022-03-23 16:29:12 -0300

    Finalizado Formulário de inserção de dados

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path
 
 from usuarios.views import index
+from visitantes.views import registrar_visitante
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path("", index)
+    path("", index, name="index",),
+
+    path("registrar_visitante", registrar_visitante, name="registrar_visitante",)
 ]
---
+++ visitantes/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import field
+from django import forms
+from visitantes.models import Visitante
+
+class VisitanteForm(forms.ModelForm):
+    class Meta:
+        model = Visitante
+        fields = "__all__"
